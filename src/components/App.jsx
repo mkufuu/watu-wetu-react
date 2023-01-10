82f702484da0427ae7ab02a1f12387a9a9779870
Mnengwa <howardmnengwa@gmail.com>
2023-01-10 16:52:25 +0300

    feat: create a <People/> component to remove repetition and add structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,8 @@
 import '../styles/app.css';
 
-import Member from './Member';
-import { useState, useEffect } from 'react';
+import People from './People';
 
 const App = () => {
-    const [people, setPeople] = useState([]);
-
-    useEffect(() => {
-        fetch("http://localhost:4000/kapenguria-six")
-            .then((resp) => resp.json())
-            .then((data) => setPeople(data))
-            .catch((error) => setPeople([]));
-    }, []);
 
     return <div className="mt-2 container-fluid">
         <header id="app-header" className="rounded">
@@ -20,13 +11,9 @@ const App = () => {
 
         <main id="app-main">
             <div className="row">
-                {
-                    people.map((member, index) => {
-                        return <div key={index} className="col-12 col-md-4 col-xl-3 mt-4 mb-2">
-                            <Member {...member} />
-                        </div>;
-                    })
-                }
+                <People endpoint="kapenguria-six" />
+
+                <People endpoint="culture" />
             </div>
         </main>
     </div>
---
+++ src/components/People.jsx
@@ -0,0 +1,33 @@
+import Member from "./Member";
+import { useState, useEffect } from "react";
+
+const People = (props) => {
+    const [members, setMembers] = useState([]);
+
+    const fetchData = async (endpoint) => {
+        try {
+            const resp = await fetch(`http://localhost:4000/${endpoint}`);
+            const data = await resp.json();
+
+            setMembers(data);
+        } catch (error) {
+            setMembers([]);
+        }
+    }
+
+    useEffect(() => {
+        fetchData(props.endpoint);
+    }, [props.endpoint]);
+
+    return <div className="row">
+        {
+            members.map((member, index) => {
+                return <div key={index} className="col-12 col-md-4 col-xl-3 mt-4 mb-2">
+                    <Member {...member} />
+                </div>;
+            })
+        }
+    </div>
+}
+
+export default People;
