29a8f363fd2a282b07b1b79c8ed5520f1a74ab28
Evelyne Atieno <117805726+eve-atieno@users.noreply.github.com>
2023-01-10 23:42:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
                 <People endpoint="kapenguria-six" />
 
                 <People endpoint="culture" />
+                    
+                <People endpoint="Limpopo" />
             </div>
         </main>
     </div>
