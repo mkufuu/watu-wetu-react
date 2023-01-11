c27548d4703e607e48a3d3691bc29db2d44dff63
melvinkukwansafari <melvin.kukwan@student.moringaschool.com>
2023-01-11 18:23:48 +0300

    adding memeber of elani band

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,8 @@ const App = () => {
                 <People endpoint="kapenguria-six" />
 
                 <People endpoint="culture" />
+
+                <People endpoint="elani" />
             </div>
         </main>
     </div>
