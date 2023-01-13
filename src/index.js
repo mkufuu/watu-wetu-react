45978a71b177c2fca43e94bf264ba524643c7f3c
Mnengwa <howardmnengwa@gmail.com>
2023-01-13 09:20:49 +0300

    feature: configure app to use React router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import './styles/index.css';
 import App from './components/App';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
 
